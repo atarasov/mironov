0b04a1046a1b22cb2ff860dca3575bb7a75e2675
Alexey Tarasov <lexa-tarasov@yandex.ru>
2013-06-03 18:28:41 +0400

    hj

--- app/controllers/cash_controller.rb
+++ app/controllers/cash_controller.rb
@@ -1,9 +1,19 @@
 class CashController < BaseController
   def index
-    if params[:year]
-      @cash = Cash.where("YEAR(DAT) = ? AND MONTH(DAT) = ? AND DAY(DAT) = ?",params[:year], Time.now.month, (Time.now - 1.day).day )
+    if params[:day]
+      @dat = Time.now - (params[:day].to_i + 1).day
     else
-      @cash = Cash.where("YEAR(DAT) = ? AND MONTH(DAT) = ? AND DAY(DAT) = ?", Time.now.year, Time.now.month, (Time.now - 1.day).day )
+      @dat = Time.now - 1.day
+    end
+    if params[:year]
+
+      @cash = Cash.where("YEAR(DAT) = ? AND MONTH(DAT) = ? AND DAY(DAT) = ?",params[:year], @dat.month, @dat.day )
+    else
+      @cash = Cash.where("YEAR(DAT) = ? AND MONTH(DAT) = ? AND DAY(DAT) = ?",@dat.year, @dat.month, @dat.day )
+      while @cash.size == 0 do
+        @cash = Cash.where("YEAR(DAT) = ? AND MONTH(DAT) = ? AND DAY(DAT) = ?",@dat.year, @dat.month, @dat.day )
+        @dat = @dat - 1.day
+      end
     end
 
     @p = Cash.select("DISTINCT YEAR(DAT) AS YEARS")
