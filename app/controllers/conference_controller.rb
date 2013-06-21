68465b59ce140f47cba69fa2a997bfe721c31e18
Alexey Tarasov <lexa.tarasov@gmail.com>
2013-06-21 22:50:01 +0400

    sdsd

--- app/controllers/conference_controller.rb
+++ app/controllers/conference_controller.rb
@@ -10,8 +10,8 @@ class ConferenceController < BaseController
     end
     @bar = LazyHighCharts::HighChart.new('Area') do |f|
       f.options[:xAxis][:categories] = []
-      f.series(:type=> 'bar', :name=>'План на день',:data=> [@plan.PLD.to_i])
-      f.series(:type=> 'bar', :name=>'План на месяц',:data=> [@plan.PLM.to_i])
+      f.series(:type=> 'bar', :legendIndex => 2, :name=>'План на день',:data=> [@plan.PLD.to_i])
+      f.series(:type=> 'bar', :legendIndex => 1, :name=>'План на месяц',:data=> [@plan.PLM.to_i])
       f.plot_options({ :bar=> {:dataLabels => { :enabled => true}}})
       f.legend({ layout: 'vertical',
                  align: 'right',
@@ -25,18 +25,22 @@ class ConferenceController < BaseController
 
 
       f.yAxis({ :min=> 0, :title=> {:text => "Тонны", :align =>'high'}})
-      f.tooltip({:shared => true,:valueSuffix => ' тонн'})
+      f.tooltip({:enabled => false,:shared => true,:valueSuffix => ' тонн'})
       f.html_options[:style] = "width:95% !important; height:200px !important;"
     end
 
     @bar2 = LazyHighCharts::HighChart.new('Area1') do |f|
       f.options[:xAxis][:categories] = []
-      f.series(:type=> 'bar', :name=>'Реализация дневная',:data=> [@plan.RLD.to_i])
-      f.series(:type=> 'bar', :name=>'Остаток дневной',:data=> [@plan.OST.to_i])
-      f.series(:type=> 'bar', :name=>'Выработка дневная',:data=> [@plan.VRD.to_i])
+
+      f.series(:type=> 'bar', :legendIndex => 3, :name=>'Реализация дневная',:data=> [@plan.RLD.to_i])
+      f.series(:type=> 'bar', :legendIndex => 2, :name=>'Остаток дневной',:data=> [@plan.OST.to_i])
+      f.series(:type=> 'bar', :legendIndex => 1, :name=>'Выработка дневная',:data=> [@plan.VRD.to_i])
       f.plot_options({ :bar=> {:dataLabels => { :enabled => true}}})
       f.yAxis({ :min=> 0, :title=> {:text => "Тонны", :align =>'high'}})
-      f.tooltip({:shared => true,:valueSuffix => ' тонн'})
+
+      f.tooltip({:shared => true,:enabled => false,
+                 :valueSuffix => ' тонн'})
+
       f.html_options[:style] = "width:95% !important; height:200px !important;"
       f.legend({ layout: 'vertical',
                  align: 'right',
@@ -58,11 +62,11 @@ class ConferenceController < BaseController
     end
     @bar = LazyHighCharts::HighChart.new('Area') do |f|
       f.options[:xAxis][:categories] = []
-      f.series(:type=> 'bar', :name=>'План нарастающий',:data=> [@plan.PLD.to_i])
-      f.series(:type=> 'bar', :name=>'План на месяц',:data=> [@plan.PLM.to_i])
+      f.series(:type=> 'bar', :legendIndex => 2, :name=>'План нарастающий',:data=> [@plan.PLD.to_i])
+      f.series(:type=> 'bar', :legendIndex => 1, :name=>'План на месяц',:data=> [@plan.PLM.to_i])
       f.plot_options({ :bar=> {:dataLabels => { :enabled => true}}})
       f.yAxis({ :min=> 0, :title=> {:text => "Тонны", :align =>'high'}, :labels =>  { :overflow => 'justify'}})
-      f.tooltip({:shared => true,:valueSuffix => ' тонн'})
+      f.tooltip({:shared => true, :enabled => false, :valueSuffix => ' тонн'})
       f.html_options[:style] = "width:95% !important; height:200px !important;"
       f.legend({ layout: 'vertical',
                  align: 'right',
@@ -77,12 +81,12 @@ class ConferenceController < BaseController
 
     @bar2 = LazyHighCharts::HighChart.new('Area1') do |f|
       f.options[:xAxis][:categories] = []
-      f.series(:type=> 'bar', :name=>'Реализация дневная',:data=> [@plan.RLD.to_i])
-      f.series(:type=> 'bar', :name=>'Остаток дневной',:data=> [@plan.OST.to_i])
-      f.series(:type=> 'bar', :name=>'Выработка дневная',:data=> [@plan.VRD.to_i])
+      f.series(:type=> 'bar', :legendIndex => 3, :name=>'Остаток дневной',:data=> [@plan.OST.to_i])
+      f.series(:type=> 'bar', :legendIndex => 2, :name=>'Выработка дневная',:data=> [@plan.VRD.to_i])
+      f.series(:type=> 'bar', :legendIndex => 1, :name=>'Реализация дневная',:data=> [@plan.RLD.to_i])
       f.plot_options({ :bar=> {:dataLabels => { :enabled => true}}})
       f.yAxis({ :min=> 0, :title=> {:text => "Тонны", :align =>'high'}})
-      f.tooltip({:shared => true,:valueSuffix => ' тонн'})
+      f.tooltip({:shared => true, :enabled => false, :valueSuffix => ' тонн'})
       f.html_options[:style] = "width:95% !important; height:200px !important;"
       f.legend({ layout: 'vertical',
                  align: 'right',
